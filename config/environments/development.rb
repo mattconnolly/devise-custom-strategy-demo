7e9a88e49edb957c12feff7d07e6c31e55683295
Matt Connolly <matt.connolly@me.com>
2012-09-23 15:00:40 +1000

    devise post install setup

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -34,4 +34,13 @@ CustomStrategy::Application.configure do
 
   # Expands the lines which load the assets
   config.assets.debug = true
+
+
+## from devise:
+#  1. Ensure you have defined default url options in your environments files. Here
+#  is an example of default_url_options appropriate for a development environment
+#  in config/environments/development.rb:
+
+  config.action_mailer.default_url_options = { :host => 'localhost:3000' }
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 CustomStrategy::Application.routes.draw do
   resources :notes
 
+  root :to => "notes#index"
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
